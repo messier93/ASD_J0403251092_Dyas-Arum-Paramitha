f1a95a9d4c49b015c2fde3442255549a0f2d626e
messier93 <dyasarum29@gmail.com>
2026-02-27 15:09:27 +0700

    Update praktikum5-latihan1.py

--- Praktikum 5/Latihan/praktikum5-latihan1.py
+++ Praktikum 5/Latihan/praktikum5-latihan1.py
@@ -1,3 +1,6 @@
+# Nama  : Dyas Arum Paramitha
+# NIM   : J0403251092
+# Kelas : TPL A2
 #===========================
 #Latihan 1: Rekursi Pangkat
 #===========================
@@ -8,5 +11,6 @@ def pangkat (a,n):
     
     #recursive case
     return a * pangkat (a, n-1)
+
 
 print(pangkat(2,4)) #output 16
