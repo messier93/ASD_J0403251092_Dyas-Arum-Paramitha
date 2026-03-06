e0eaae74bd354b7928dc8b95ae296a6a269eafc9
messier93 <dyasarum29@gmail.com>
2026-03-06 15:14:54 +0700

    Update bubbleSort-asc.py

--- Praktikum 6/bubbleSort-asc.py
+++ Praktikum 6/bubbleSort-asc.py
@@ -3,12 +3,14 @@
 # Kelas : TPL A2
 
 # =========================
-# Bubble Sort (Descending)
+# Bubble Sort (Ascending)
 # =========================
 
 def bubbleSort(data):
+    #compare elemen yang bersebelahan
     for passnum in range(len(data)-1,0,-1):
         for i in range(passnum):
+            #jika elemen kiri lebih besar dari kanan maka tukar
             if data[i]>data[i+1]:
             #tukar dua data bersebalahan yg urutannya salah
                 temp = data[i]
@@ -18,3 +20,32 @@ def bubbleSort(data):
 data = [54,26,93,17,77,31,44,55,20]
 bubbleSort(data)
 print(data)
+
+#Program 4
+def shortBubbleSort(alist):
+    exchanges = True
+    
+    passnum = len(alist)-1
+    
+    # loop berjalan selama masih ada pertukaran
+    while passnum > 0 and exchanges:
+        exchanges = False
+        
+        for i in range(passnum):
+            # jika urutan salah maka tukar
+            if alist[i] > alist[i+1]:
+                exchanges = True
+                
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        
+        passnum = passnum-1
+
+alist=[20,30,40,90,50,60,70,80,100,110]
+
+shortBubbleSort(alist)
+
+print(alist)
+
+#Output [17, 20, 26, 31, 44, 54, 55, 77, 93]
