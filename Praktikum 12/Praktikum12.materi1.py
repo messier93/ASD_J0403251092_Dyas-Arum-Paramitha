2454d143b8e09ab44d0ef96b42c8502838e2ab6d
messier93 <dyasarum29@gmail.com>
2026-05-22 15:24:08 +0700

    Update Praktikum12.materi1.py

---
+++ Praktikum 12/Praktikum12.materi1.py
@@ -0,0 +1,17 @@
+# Nama    : Dyas Arum Paramitha
+# NIM     : J0403251092
+# Kelas   : A2
+
+# ==========================================================
+# Materi 1: Konsep Weighted Graph
+# ==========================================================
+
+'''Pada Python, weighted graph dapat direpresentasikan menggunakan dictionary bersarang
+(nested dictionary).
+Contoh:'''
+
+graph = {
+'A': {'B': 4, 'C': 2},
+'B': {'C': 1},
+'C': {}
+}
